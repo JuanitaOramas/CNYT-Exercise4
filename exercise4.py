5e36fd0cd6f9fff25170d1a0abac26bdbaa42c73
JuanitaOramas <77862004+JuanitaOramas@users.noreply.github.com>
2022-03-27 22:33:01 -0500

    Update exercise4.py

--- exercise4.py
+++ exercise4.py
@@ -1,1 +1,25 @@
+def probability (pos,v):
+    a=normVector(v[pos])
+    b=normVector(v)
+    probability=(a/b)
+    return probability*100
+  
+def VectorProduct(v,v1):
+    answer = 0 
+    for i in range(len(v)):
+        answer+=v[i]*v1[i]
+    return answer
+  
+  
+# Exercise # 4.4.1
+def UnitaryVerify(u1,u):
+    
+    if unitMatrix(m)(U1) and unitMatrix(m)(u):
+        answer =  matrixProduct(u1,u)
+        return unitMatrix(answer)
 
+# Exercise # 4.4.2 
+def Experiment(v,n,clicks):
+    for i in range(clicks):
+        v = multiScalarMatrixCplx(v,n)
+    return v
